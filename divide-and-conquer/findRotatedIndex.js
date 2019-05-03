fc6ccc02b121be5459f20757873493fbb9656443
Jonathan Yuen <jonwyuen@gmail.com>
2019-05-03 00:03:22 -0700

    findRotatedIndex

--- divide-and-conquer/findRotatedIndex.js
+++ divide-and-conquer/findRotatedIndex.js
@@ -14,11 +14,42 @@
 // findRotatedIndex([6, 7, 8, 9, 1, 2, 3, 4], 12) // -1
 
 const findRotatedIndex = (arr, num) => {
+  let pivot = findPivot(arr);
+  if (pivot > 0 && num >= arr[0] && num <= arr[pivot - 1]) {
+    return binarySearch(arr, num, 0, pivot - 1);
+  } else {
+    return binarySearch(arr, num, pivot, arr.length - 1);
+  }
+};
 
-}
+const findPivot = arr => {
+  if (arr.length === 1 || arr[0] < arr[arr.length - 1]) return 0;
+  let start = 0;
+  let end = arr.length - 1;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] > arr[mid + 1]) return mid + 1;
+    else if (arr[start] <= arr[mid]) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+};
 
-const findLowestIndex = (arr, start=0, end=arr.length-1) {
+const binarySearch = (arr, num, start = 0, end = arr.length - 1) => {
+  if (arr.length === 0) return -1;
+  if (num < arr[start] || num > arr[end]) return -1;
 
-  let mid = Math.floor((start+end)/2);
-  
-}
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+    if (arr[mid] === num) {
+      return mid;
+    } else if (num < arr[mid]) {
+      end = mid - 1;
+    } else {
+      start = mid + 1;
+    }
+  }
+  return -1;
+};
